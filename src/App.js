09c89f47ae396f76e4f6ab5cecbbbdf0f8efb32a
Nandu 97 <mysurunandakishore@gmail.com>
2020-05-09 22:57:04 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Form from './Form';
+import Navbar from './Navbar';
+const App=()=> {
+  return (
+    <div >
+      <Navbar/>
+      <h1 className="text-center">Rent our Cars in this platform! </h1>
+      <h1 className="text-center"> Go Check it out ! </h1>
+    </div>
+  );
+}
+
+export default App;
